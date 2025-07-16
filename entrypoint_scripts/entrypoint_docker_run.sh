02dad0481381c77a03558995ec1366c71a75e7d0
Abhishek <deshmukhabhi02@gmail.com>
2025-07-16 13:40:45 +0200

    #28: Update UR5 Config and IsaacLab

--- entrypoint_scripts/entrypoint_docker_run.sh
+++ entrypoint_scripts/entrypoint_docker_run.sh
@@ -3,4 +3,6 @@
 # Source the ROS2 Workspace
 source /opt/ros/humble/setup.bash
 
+bash /workspace/isaaclab/isaaclab.sh -p -m pip install -e /rl_robotics/isaac_workspace/UR5_direct/source/UR5_direct/
+
 bash
